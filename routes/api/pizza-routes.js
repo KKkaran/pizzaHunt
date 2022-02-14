2a63202ebe8033cddf1e25c945fd659612c51414
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2022-02-14 13:37:14 -0500

    able to create pizza

---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,22 @@
+const {
+    getAllPizza,
+    getPizzaById,
+    createPizza,
+    updatePizza,
+    deletePizza
+  } = require('../../controllers/pizza-controllers');
+const router = require('express').Router()
+
+router.route('/')
+      .get(getAllPizza)
+      .post(createPizza)
+
+
+
+router.route('/:id')
+      .get(getPizzaById)
+      .put(updatePizza)
+      .delete(deletePizza)
+
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const htmlRoutes = require('./html/html-routes');
+const apiROutes = require("./api")
 
 router.use('/', htmlRoutes);
+router.use('/api', apiROutes);
 
 router.use((req, res) => {
   res.status(404).send('<h1>😝 404 Error!</h1>');
